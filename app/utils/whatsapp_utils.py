bb4cc88d494bd331fe429667def14470e59f95bc
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-03-26 10:39:05 +0200

    Update WhatsApp AirBnb Assistant code

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -11,6 +11,7 @@ from openai import ChatCompletion
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from colorama import Fore, Style
 
 Base = declarative_base()
 
@@ -51,7 +52,7 @@ def create_subscription(mobile_number,message, subscription_status):
             subscription_status=subscription_status,
             message=message,
             trial_start_date=today,
-            trial_end_date=today + timedelta(days=7),
+            trial_end_date=today - timedelta(days=7),
         )
         session.add(subscription)
         session.commit()
@@ -82,11 +83,12 @@ def generate_response(response, wa_id, name):
     global conversation
     Subscription_status = create_subscription(wa_id[0], response, "Free Trial")
     conversation.append({"role": "user", "content": response})
-    if Subscription_status == "expired":
-        response = "Your free trial has expired. Please subscribe to continue using the service."
+    
+    if Subscription_status == "created":
+        response = f" Hi {name}\nYour Free trial subscription has been created. You have a free trial for 7 days. expiring on {today + timedelta(days=7)}.\n Regards,\n *tnqn*."
         return response
-    elif Subscription_status == "created":
-        response = f"Your Free trial subscription has been created. You have a free trial for 7 days. expiring on {today + timedelta(days=7)}.\n Regards, tnqn."
+    elif Subscription_status == "expired":
+        response = " ⚠️  *YOUR FREE TRIAL HAS EXPIRED*\nPlease Choose SUbscription Option\n1. Monthly Subscription\n2. Cancel Subscription\n3. Check Subscription Status\n4. Help\n5. Exit\n\n *Please reply with the number of your choice*."
         return response
     else:
         if response.lower().endswith("bypasslimit"):
@@ -101,7 +103,6 @@ def generate_response(response, wa_id, name):
                 stop=None
             )
             conversation.append({"role": "assistant", "content": response.choices[0].message.content.strip("bypasslimit")})
-
         elif response.lower() in ["who are you?", "what is your name", "where are you from", "who made you?", "who is tankan", "tankan"]:
             response = "I am tankan's assistant. I am here to help you with anything you need."
             return response
@@ -198,3 +199,48 @@ def is_valid_whatsapp_message(body):
         and body["entry"][0]["changes"][0]["value"].get("messages")
         and body["entry"][0]["changes"][0]["value"]["messages"][0]
     )
+
+def colorize_text(text, color):
+    
+    colored_text = f"{color}{text}{Style.RESET_ALL}"
+    return colored_text
+
+def activate_subscription(wa_id,message):
+    subs_activity = create_subscription(wa_id, "", "")
+    subscription_detail = session.query(Subscription).filter_by(mobile_number=wa_id).first()
+    if subs_activity == "expired":
+        if message == 1 or message=="1.":
+            response = "Monthly Subscription Plan:\n\n*Features*\n1. Unlimited Access to all features\n2. 24/7 Customer Support\n3. Cancel Anytime\n\n*Pricing*\n$1/month\n\n*To subscribe, please reply with, *Y* to proceed or *N* to abort.*"
+            return response
+        elif message == 2 or message=="2.":
+            response = "Your subscription has been cancelled. To reactivate, please reply with *1* to subscribe."
+            return response
+        elif message == 3 or message=="3.":
+            response = f"Your subscription has expired on {subscription_detail.trial_end_date}. To reactivate, please reply with *1* to subscribe."
+            return response
+        elif message == 4 or message=="4.":
+            response = "Thank you for your interest in our subscription plans. We offer convenient options to help you split service costs efficiently.\n\n"
+            response += "Subscription Plans:\n"
+            response += "- Monthly Subscription Plan\n"
+            response += "- Cancel Subscription\n\n"
+            response += "By subscribing, you will gain unlimited access to all features and enjoy 24/7 customer support.\n\n"
+            response += "To proceed with the Monthly Subscription Plan, please reply with *1*. To cancel your subscription, please reply with *N*.\n\n"
+            response += "For any inquiries or assistance, please contact our support team.\n\n"
+            response += "© 2023 TechProjectsHub. All rights reserved.\n"
+            return response
+        elif message == 5 or message=="5.":
+            response = "Thank you for using our service. If you need any further assistance, please feel free to contact us. Have a great day!\n\n 🫰"
+            return response
+        if message.lower() == "y":
+            response = "Your subscription is being created. You will be billed $1/month. To cancel your subscription, please reply with *2* or reply with your ecocash mobile number to proceed."
+            return response
+
+        if len(message) >9  and type(message) == int:
+            pattern = r'^(077|078)\d{7}$'
+            match = re.match(pattern, message)
+            if match:
+                response = "Please wait for the popup on your phone to confirm the payment. Your subscription will be activated once the payment is confirmed."
+                return response
+            else:
+                response = "Invalid Ecocash number. Please reply with a valid Ecocash number to proceed."
+                return response
