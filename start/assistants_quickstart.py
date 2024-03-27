7083e36cb364eb5d5edde8e7cde3b7f591f5a7f2
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-03-27 07:49:14 +0200

    Fix commented out code and update payment details

--- start/assistants_quickstart.py
+++ start/assistants_quickstart.py
@@ -11,30 +11,6 @@ paynow = Paynow(
     'https://www.paynow.co.zw', 
     'http://google.com'
     )
-# payment = paynow.create_payment('Order #100', 'gtkandeya@gmail.com')
-# payment.add('Bananas', 7)
-# response = paynow.send_mobile(payment, '0779586059', 'ecocash')
-# if(response.success) :
-#     # Get the poll url (used to check the status of a transaction). You might want to save this in your DB
-#     poll_url = response.poll_url
-#     instructions = response.instructions
-#     status = paynow.check_transaction_status(poll_url)
-
-#     if status.paid :
-#         print('Yay! Transaction was paid for. Update transaction?')
-#         # Yay! Transaction was paid for. Update transaction?
-#         pass
-#     else :
-#         print('Boo! Transaction ')
-#         pass
-
-#         # Boo! Transaction wasn't paid for. Maybe ask the user to try again?
-# else :
-#     print('Error: %s' % response.error)
-#     # Error: Invalid integration ID
-#     pass
-
-
 
 payment = paynow.create_payment('Order', 'gtkandeya@gmail.com')
 
