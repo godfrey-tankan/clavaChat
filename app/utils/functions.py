0b80373fc4d02b59f269087101e48245bd968e40
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-04-15 15:37:27 +0200

    Refactor search_document function in functions.py to improve efficiency and handle errors

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -18,9 +18,7 @@ from flask import send_file
 
 def search_document(document_name):
     current_directory = os.getcwd() 
-    print("current_directory:", current_directory)
     folder_path = current_directory + "/app/utils/documents"
-    print("folder_path:", folder_path)
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.lower() == document_name.lower():
@@ -33,23 +31,18 @@ def library_contents_lookup(requester, message):
     document_path = search_document(message)
     if document_path:
         print("Document found at:", document_path)
-        try:
-            requested_document_by_user = session.query(Document).filter(and_(Document.title == message, Document.file_path == requester)).first()
-            if requested_document_by_user:
-                # return send_file(document_path, mimetype='application/pdf')
-                response = send_file(document_path, attachment_filename=message, as_attachment=True)
-                print(response, "response", type(response))
-                return response
-            else:
-                requested_document = Document(title=message, category="Library", file_path=requester,mimetype='application/pdf')
-                session.add(requested_document)
-                session.commit()
-                response = send_file(document_path, attachment_filename=message, as_attachment=True)
-                print(response, "response", type(response))
-                return response
+        # try:
+        requested_document_by_user = session.query(Document).filter_by(title = message, file_path =requester).first()
+        if requested_document_by_user:
+            pass
+        else:
+            requested_document = Document(title=message, category="Library", file_path=requester)
+            session.add(requested_document)
+            session.commit()
+        return send_file(document_path, attachment_filename=message, as_attachment=True)
 
-        except FileNotFoundError as e:
-            # Handle the FileNotFoundError appropriately
-            return f"error somewhere..{e}"
+        # except FileNotFoundError as e:
+        #     # Handle the FileNotFoundError appropriately
+        #     return f"error somewhere..{e}"
     else:
-        return None
+        return "Document not found! Please check the document name and try again."
