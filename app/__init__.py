23a78bcbf9620fd787fbe2ec58c0a4740e6187bb
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-05-01 14:30:59 +0200

    Refactor app/__init__.py to remove unnecessary code and improve readability

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,11 @@ from app.config import load_configurations, configure_logging
 from .views import webhook_blueprint
 
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Load configurations and logging settings
-    load_configurations(app)
-    configure_logging()
+# Load configurations and logging settings
+load_configurations(app)
+configure_logging()
 
-    # Import and register blueprints, if any
-    app.register_blueprint(webhook_blueprint)
-
-    return app
+# Import and register blueprints, if any
+app.register_blueprint(webhook_blueprint)
