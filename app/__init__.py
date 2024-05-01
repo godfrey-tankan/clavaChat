47b0979a7ea50475c201db64d89ec75f4cae72bf
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-05-01 14:52:53 +0200

    Add run.py file to start the Flask app and update the welcome route

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from app.config import load_configurations, configure_logging
 from .views import webhook_blueprint
 
-
 app = Flask(__name__)
 
 # Load configurations and logging settings
@@ -11,3 +10,7 @@ configure_logging()
 
 # Import and register blueprints, if any
 app.register_blueprint(webhook_blueprint)
+
+if __name__ == '__main__':
+    # Run the app on port 8000
+    app.run(host='0.0.0.0', port=8000)
