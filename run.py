3755398b945a812bec23dbcc80104fc5308da254
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-05-01 13:53:26 +0200

    Refactor run.py to add a welcome route

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from subprocess import Popen
 from app import create_app
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/')
+def hello():
+    return "Welcome to clavaChat!"
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
    
