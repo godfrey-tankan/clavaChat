741e9a01bd3a51211d4f9d43c06a45a37f3d1588
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-05-22 00:24:15 +0200

    Refactor code to update OPENAI_API_KEY

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -18,7 +18,7 @@ from app.services.chat_responses import *
 from app.services.user_types import *
 from app.config import *
 
-openai.api_key = "sk-proj-8QY5FrIOMthVxADMNuzAT3BlbkFJ0Dbkm4dMFPlfdYXonOE6"
+openai.api_key = "sk-proj-NJ8KaBnTT1hYdEZLhbAXT3BlbkFJGn6wyQdeUMTNZ5KA7UQQ"
 conversation = []
 today = datetime.now().date()
 
