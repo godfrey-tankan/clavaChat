ca20b6616dcd51df05de40c4d13967f280ee4779
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-06-01 16:40:28 +0200

    Refactor invalid product information response for better clarity and consistency

--- app/services/chat_responses.py
+++ app/services/chat_responses.py
@@ -16,7 +16,7 @@ invalid_house_information ="⚠️Invalid house information. \n\n- Please provid
 landlord_name_response = "We would like to use your name when referring tenants to you.\nPlease provide your Name or Company Name"
 no_houses_found_response = "We couldn't find any listing, try another location."
 no_products_found_response = "Your search did not match any products. Please check the spelling or try a different product."
-invalid_product_information ="⚠️Invalid product information provided. \n\n- Please provide:\n- *Product name* \n- *Condition* \n- *Budget* \n\nYou can consider this example:*iphone13 pro-max 256GB,boxed $1200* \nreply with `exit` to exit."
+invalid_product_information ="⚠️Invalid product information provided. \n\n- Please provide:\n- *Product name* \n- *Condition* \n- *Budget - `budget is optional`* \n\nYou can consider this example:*iphone 13 256GB,boxed $1200* \nreply with `exit` to exit."
 questions_list =["who are you?", "what is your name", "where are you from", "who made you?", "who is tankan", "tankan"]
 
 library_response = "Welcome to the `clavaChat` Library.\n\n- -> *If looking for a novel/book 📖:*,\n- reply with the title.\n\n- -> If it's a past exam paper\n- `consider this example`: *A level mathematics paper 1 November 2023*.\n\n ⚠️*DISCLAIMER* Please `note` that responses may not be accurate!\n\nType `exit` to exit."
@@ -43,7 +43,7 @@ subs_error_response = "An error occurred. Please try again later."
 subs_cancel_response = "Your subscription has been cancelled. To reactivate, please reply with *1* to subscribe."
 landlord_subs_response = "Landlord Subscription Plan:\n\n *Features* \n1. Manage all your properties\n2. 24/7 Customer Support\n3. Cancel Anytime - `guaranteed money back within first week` if you decided to change otherwise.\n\n *Pricing List* \n- 1. $5.77/monthly\n- 2. $13.80/quarterly\n- 3. $22.00/half-yearly\n- 4. $39.90/Yearly  \n\n To subscribe, please reply with your subscription choice to proceed or exit to exit."
 seller_subs_response = "Seller Subscription Plan:\n\n *Features* \n1. Manage all your products\n2. 24/7 Customer Support\n3. Cancel Anytime - `guaranteed money back within first week` if you decided to change otherwise.\n\n *Pricing List* \n- 1. $1/monthly\n- 2. $2.30/quarterly\n- 3. $4/half-yearly\n- 4. $7/Yearly  \n\n To subscribe, please reply with your subscription choice to proceed or exit to exit."
-landlord_proceed_with_subs_response = "Your *{}* subscription is being created.Subscription amount is $`{}`. Please reply with your Ecocash mobile number in the form: `07XX` to proceed.\n\nTo cancel your subscription, please reply with *exit*."
+landlord_proceed_with_subs_response = "Your *{}* subscription is being created.Subscription amount is $ `{}`. Please reply with your Ecocash mobile number in the form: `07XX` to proceed.\n\nTo cancel your subscription, please reply with *exit*."
 landlord_monthly_subs_response = "Your subscription is on {}"
 
 #Payment section
